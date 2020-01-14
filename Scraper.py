88427c8ae86251ead7c872b3935c694eed3cee9d
amshel <amshelhack3r@gmail.com>
2020-01-14 19:05:01 +0000

    crreate a Scraper class to handle scraping

--- Scraper.py
+++ Scraper.py
@@ -1,24 +1,59 @@
 from bs4 import BeautifulSoup
 import requests
+from phoneinfo import PhoneInfo
 
-redmi = "https://www.jumia.co.ke/redmi-note-7-pro-6.30-inch-fhd6128gb-4000mah-smartphone-black-26898382.html"
+
+class Scraper():
+    base_uri = "https://jumia.co.ke/"
+    
+    def __init__(self, value):
+        value = value.split()
+        self.model = value[0]
+        value.pop(0)
+        
+        # means only one value was passed to the query eg "huawei"
+        if len(value) == 0:
+            self.base_uri += self.model
+            pass
+        # means that more than 1 values were passed to the query eg "huawei y9" or "huawei y9 4gb 64gb"
+        else:
+            self.base_uri = Scraper.urlBuilder(self.model, *value)
+            pass
+
+        print(self.base_uri)
+        self.buildList()
 
 
-def getSoup(url):
-    html = requests.get(url)
-    return BeautifulSoup(html.content, 'html.parser')
+    def buildList(self):
+        soup = Scraper.getSoup(self.base_uri)
+        products = soup.find("section", attrs={"class":"products"})
+        gallery = products.find_all("a", limit=10)
+        
+        for elem in gallery:
+            link = elem.get("href")
+            image = elem.find("img").get("data-src")
+            name = elem.find("span", attrs={"class": "name"}).string
+            price_div = elem.find("span", attrs={"class":"price"})
+            price = price_div.find("span", attrs={"dir":"ltr"}).string
 
-soup = getSoup(redmi)
-name = soup.find("h1").string
-# scrap image
-#the first radio button
-radio = soup.find("input", attrs={"id":"imgs-sld-1"})
-link = radio.find_next_sibling("a")
-image = link.find("img")
+            print('*********************EOL***********************')
+            print(name)
+            print(price)
+            print(image)
+            print(link)
+            print('*********************EOL***********************')
 
-# scrap the price 
-price= soup.find("span", attrs={"dir":"ltr"})
-print(price.string)
+    @classmethod        
+    def getSoup(cls, url):
+        html = requests.get(url)
+        return BeautifulSoup(html.content, 'html.parser')
+        
 
-# print(image.get("data-src"))
+    @classmethod
+    def urlBuilder(cls, model, *args):
+        query_string = ""
+        for word in args:
+            query_string += f'{word}+'
+        pass
 
+        return f'{Scraper.base_uri}{model}/?q={query_string}'
